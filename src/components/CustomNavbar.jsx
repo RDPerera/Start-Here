2c1c4bb2c5e1b84c7c183b5087dd34afbf6b6d8d
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-10-26 01:30:41 +0530

    nav

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import { Link, NavLink } from "react-router-dom";
+import { Button } from "arwes";
 
 function CustomNavbar(props) {
   return (
-    <div></div>
+    <div>
+      <Button animate>Cyberpunk</Button>
+      <Button animate>Cyberpunk</Button>
+      <Button animate>Cyberpunk</Button>
+      <Button animate>Cyberpunk</Button>
+      <Button animate>Cyberpunk</Button>
+    </div>
     // <Navbar bg="dark" variant="dark">
     //   <Navbar.Brand as={Link} to="/">
     //     LAKSHAN
