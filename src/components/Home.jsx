b591e5052b6a9057d0e9199cc1ad91ee821d7449
Nandula Perera <pereranandula1999@gmail.com>
2020-10-26 04:26:54 +0530

    Add server.js

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import AdList from "./AdList";
 import { Heading, Row } from "arwes";
 import Loading from "arwes/lib/Loading";
+import logo from '../assets/starthere.png';
 
 class Home extends React.Component {
   render() {
     return [
       <div>
-        <Loading />
-        <Heading node="h1" class="heading">
-          Start Here
-        </Heading>
+        <div style={{textAlign: "center"}}><img src={logo}/></div>
         <Row>
           <AdList />
         </Row>
