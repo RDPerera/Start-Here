18bf46c0ddd5e4aeecfa8022fbaa24d270bd67a2
Dilan Perera <r.dilanperera@gmail.com>
2020-10-26 11:47:09 +0530

    Sound Added

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,37 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
 
+import click from "./sounds/click.mp3";
+import type from "./sounds/typing.mp3";
+import deploy from "./sounds/deploy.mp3";
+
 import About from "./components/About";
 import Contact from "./components/Contact";
 
 import AppInfo from "./components/AppInfo";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import { SoundsProvider, createSounds, Button } from "arwes";
+
+const mySounds = {
+  shared: { volume: 1 }, // Shared sound settings
+  players: {
+    // The player settings
+    click: {
+      // With the name the player is created
+      sound: { src: [click] }, // The settings to pass to Howler
+    },
+    typing: {
+      sound: { src: [type] },
+      settings: { oneAtATime: true }, // The custom app settings
+    },
+    deploy: {
+      sound: { src: [deploy] },
+      settings: { oneAtATime: true },
+    },
+  },
+};
 
 class App extends React.Component {
   render() {
@@ -45,12 +70,14 @@ class App extends React.Component {
     return (
       <ThemeProvider theme={createTheme()}>
         <Arwes background={background} pattern={pattern}>
+        <SoundsProvider sounds={createSounds(mySounds)}>
           <Puffs>
             <Router>
               <div>{displayContent()}</div>
             </Router>
           </Puffs>
-        </Arwes>
+        </SoundsProvider>
+        </Arwes>  
       </ThemeProvider>
     );
   }
