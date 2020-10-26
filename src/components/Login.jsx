afe46806cbd80986c2da2a2f8831d9d811a0bc31
georgeraveen <georgeraveen@gmail.com>
2020-10-26 17:11:19 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,9 +2,37 @@ import React from 'react';
 import './login.css';
 import { Frame, Button } from 'arwes';
 import TextBox from "./TextBox";
+import firebase from "firebase";
 import logo from '../assets/starthere.png';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.changeEmail=this.changeEmail.bind(this);
+    this.changePass=this.changePass.bind(this);
+
+    this.state={
+      email:"",
+      pass:"",
+    };
+  }
+  handleClick() {
+    console.log(this.state);
+    firebase.auth().createUserWithEmailAndPassword(this.state.email,this.state.pass);
+    
+  }
+  changeEmail(evt) {
+    console.log(evt.target.value);
+    this.setState({
+      email:evt.target.value
+    });
+  }
+  changePass(evt) {
+    console.log(evt.target.value);
+    this.setState({
+      pass:evt.target.value
+    });
+  }
   render() {
     return [
         <div class="form">
@@ -18,9 +46,14 @@ class Login extends React.Component {
             >
                 <div style = {{textAlign: "center"}}>
                     <img src={logo} alt="StartHere Logo" style = {{width: "75%"}}/>
-                    <TextBox type="text" placeholder="Username" top="5%" bottom="5%"></TextBox>
-                    <TextBox type="password" placeholder="Password" top="5%" bottom="5%"></TextBox>
-                    <Button animate layer='primary' style={{width: "100%"}}>
+                    <TextBox type="text" placeholder="Username" top="5%" bottom="5%"
+                                  onNameChange={this.changeEmail}
+                           ></TextBox>
+                    <TextBox type="password" placeholder="Password" top="5%" bottom="5%" 
+                      onNameChange={this.changePass}
+                    
+                    ></TextBox>
+                    <Button animate layer='primary' style={{width: "100%"}} onClick={() => this.handleClick()}>
                         <i className='mdi' /> Login 
                     </Button>
                 </div>
