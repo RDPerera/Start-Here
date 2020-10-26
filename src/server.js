a4f6567077e83f02d354828fe9020ff3fad79d4b
Nandula Perera <pereranandula1999@gmail.com>
2020-10-26 08:50:58 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const favicon = require('express-favicon');
 const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
-app.use(express.static(__dirname + '/build'));
+
+app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/build/index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(process.env.PORT || 8080);
+
+app.listen(port);
